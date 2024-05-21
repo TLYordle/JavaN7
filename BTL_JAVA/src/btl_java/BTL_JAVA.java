000b8d1ffb23bd3eb8c45fee56d72fcd2eb7968d
Phungngoccoc <154677053+Phungngoccoc@users.noreply.github.com>
2024-05-21 21:27:17 +0700

    ngoc

--- BTL_JAVA/src/btl_java/BTL_JAVA.java
+++ BTL_JAVA/src/btl_java/BTL_JAVA.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Test;
+package btl_java;
 
 /**
  *
  * @author FPTSHOPKM4
  */
-public class Test {
+public class BTL_JAVA {
 
     /**
      * @param args the command line arguments
