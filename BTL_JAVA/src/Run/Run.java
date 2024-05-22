daf9f17c5efc2a4f12b0b121565404b2e903048a
Phungngoccoc <154677053+Phungngoccoc@users.noreply.github.com>
2024-05-22 16:14:10 +0700

    phungthengoc

---
+++ BTL_JAVA/src/Run/Run.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Run;
+
+/**
+ *
+ * @author FPTSHOPKM4
+ */
+public class Run {
+    public static void main(String[] args)
+    {
+        
+    }
+}
