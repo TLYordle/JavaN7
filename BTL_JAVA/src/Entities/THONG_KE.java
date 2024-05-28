ab3db762679040a052ed889069553f18d162da53
LocReindeerB23 <147324953+LocReindeerB23@users.noreply.github.com>
2024-05-28 13:41:56 +0700

    Nguyễn Tuần Lộc

--- BTL_JAVA/src/Entities/THONG_KE.java
+++ BTL_JAVA/src/Entities/THONG_KE.java
@@ -3,11 +3,73 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Entities;
-
+import java.util.Date;
+import Tools.DatabaseToList;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.io.IOException;
 /**
  *
  * @author FPTSHOPKM4
  */
-public class THONG_KE {
-    
+public class THONG_KE implements Serializable {
+    private String maDanhGia;
+   private String cccd;
+   private String maThongKe;
+   private String maNV;
+   private Date ngayThongKe;
+
+    public THONG_KE(String maDanhGia, String cccd, String maThongKe, String maNV, Date ngayThongKe) {
+        this.maDanhGia = maDanhGia;
+        this.cccd = cccd;
+        this.maThongKe = maThongKe;
+        this.maNV = maNV;
+        this.ngayThongKe = ngayThongKe;
+    }
+
+    public String getMaDanhGia() {
+        return maDanhGia;
+    }
+
+    public void setMaDanhGia(String maDanhGia) {
+        this.maDanhGia = maDanhGia;
+    }
+
+    public String getCccd() {
+        return cccd;
+    }
+
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
+    }
+
+    public String getMaThongKe() {
+        return maThongKe;
+    }
+
+    public void setMaThongKe(String maThongKe) {
+        this.maThongKe = maThongKe;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public Date getNgayThongKe() {
+        return ngayThongKe;
+    }
+
+    public void setNgayThongKe(Date ngayThongKe) {
+        this.ngayThongKe = ngayThongKe;
+    }
+
+    @Override
+    public String toString() {
+        return "THONG_KE{" + "maDanhGia=" + maDanhGia + ", cccd=" + cccd + ", maThongKe=" + maThongKe + ", maNV=" + maNV + ", ngayThongKe=" + ngayThongKe + '}';
+    }
 }
