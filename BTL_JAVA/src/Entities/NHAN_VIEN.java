4d73cc92c24c3e57470d9c94ebed872c7e0e354a
Manh31 <kirishimam47@gmail.com>
2024-05-29 10:48:01 +0700

    NHAN_VIEN.java

--- BTL_JAVA/src/Entities/NHAN_VIEN.java
+++ BTL_JAVA/src/Entities/NHAN_VIEN.java
@@ -4,10 +4,70 @@
  */
 package Entities;
 
+import Tools.DatabaseToList;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.io.IOException;
+
 /**
  *
  * @author FPTSHOPKM4
  */
-public class NHAN_VIEN {
+public class NHAN_VIEN implements Serializable{
+    private String MaNV;
+    private String tenNV;
+    private String TrangThai;
+    private String CCCD;
+
+    public NHAN_VIEN() {
+    }
+
+    public NHAN_VIEN(String MaNV, String tenNV, String TrangThai, String CCCD) {
+        this.MaNV = MaNV;
+        this.tenNV = tenNV;
+        this.TrangThai = TrangThai;
+        this.CCCD = CCCD;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public String gettenNV() {
+        return tenNV;
+    }
+
+    public void settenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(String TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+
+    public String getCCCD() {
+        return CCCD;
+    }
+
+    public void setCCCD(String CCCD) {
+        this.CCCD = CCCD;
+    }
+
+    @Override
+    public String toString() {
+        return "NHAN_VIEN{" + "MaNV=" + MaNV + ", tenNV=" + tenNV + ", TrangThai=" + TrangThai + ", CCCD=" + CCCD + " '}';
+    }
     
+    public static void main(String[] args) throws SQLException{
+       ArrayList<NHAN_VIEN> danhSachnhanvien = DatabaseToList.Doc_nhanvien_Tu_CSDL();
+    }
 }
