90abfb203fd4269209861a85c764f84963217205
LocReindeerB23 <147324953+LocReindeerB23@users.noreply.github.com>
2024-05-30 14:47:52 +0700

    Nguyễn Tuần Lộc

--- BTL_JAVA/src/Entities/DANH_GIA.java
+++ BTL_JAVA/src/Entities/DANH_GIA.java
@@ -9,5 +9,25 @@ package Entities;
  * @author FPTSHOPKM4
  */
 public class DANH_GIA {
-    
+     public AbstractStringBuilder getMaDG() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setMaDG(String trim) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setNdDG(String trim) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setCccd(String trim) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static class AbstractStringBuilder {
+
+        public AbstractStringBuilder() {
+        }
+    }
 }
